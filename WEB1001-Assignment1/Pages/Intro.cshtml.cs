ddd9202a1d65f07e5b36c29d76e82038c1dafa51
Fikrath Hassan <fikrathhassan@users.noreply.github.com>
2022-02-20 17:26:23 +0530

    added form into Intro page

---
+++ WEB1001-Assignment1/Pages/Intro.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WEB1001_Assignment1.Pages
+{
+    public class IntroModel : PageModel
+    {
+        [FromQuery(Name = "number1")]
+        public Double Number_1
+        {
+            get;
+            set;
+        }
+
+        [FromQuery(Name = "number2")]
+        public Double Number_2
+        {
+            get;
+            set;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
